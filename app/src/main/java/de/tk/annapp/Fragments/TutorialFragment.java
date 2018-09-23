f7a5714f076c9fdf51adfd71537d06b3af52f740
TxxxxxKXxxxxxxx <tau.kappa.2000@gmail.com>
2018-09-23 18:56:34 +0200

    nav drawer fixed (during mainActivity)

--- app/src/main/java/de/tk/annapp/Fragments/TutorialFragment.java
+++ app/src/main/java/de/tk/annapp/Fragments/TutorialFragment.java
@@ -117,6 +117,8 @@ public class TutorialFragment extends Fragment{
                     getActivity().findViewById(R.id.include).findViewById(R.id.toolbar).setVisibility(View.VISIBLE);
 
                     getActivity().findViewById(R.id.nav_view).setVisibility(View.VISIBLE);
+
+                    ((DrawerLayout)getActivity().findViewById(R.id.drawer_layout)).setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
                 }
             }
         });
